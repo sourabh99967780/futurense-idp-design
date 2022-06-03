3f62d3ac439e6ff48d60fe1d5bcecfaace0e52cb
Sourabh Kumar <sourabhkumar@Sourabhs-MacBook-Air.local>
2022-06-04 00:46:35 +0530

    Dasdhboard Design almost complete

---
+++ assets/js/dashboard.js
@@ -0,0 +1,32 @@
+$('.toggle-icon').click(() => {
+    $('#dashboard').toggleClass('sidebar-toggled');
+})
+
+// set sidebar height dynamically
+$('#dashboard .sidebar-wrapper').height('calc(100vh - ' + $('#dashboard header').height() + 'px)');
+
+const slider = document.querySelector('#dashboard .external__links');
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+};
+let stopDragging = function (event) {
+  mouseDown = false;
+};
+
+slider.addEventListener('mousemove', (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+});
+
+// Add the event listeners
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
--- assets/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-$('.toggle-icon').click(() => {
-    $('#dashboard').toggleClass('sidebar-toggled');
-})
-
-// set sidebar height dynamically
-$('#dashboard .sidebar-wrapper').height('calc(100vh - ' + $('#dashboard header').height() + 'px)');
